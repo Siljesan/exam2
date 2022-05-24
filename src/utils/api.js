b3b47ec8510a56a6f61eb907ec75d789484ee4cd
Siljesandersen <silje_sandersen@hotmail.com>
2022-05-24 11:28:44 +0200

    fix: updated api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:1337/";
+export const BASE_URL = "https://project-exam-api.herokuapp.com/";
 export const ESTABLISHMENT_URL = `${BASE_URL}api/establishments/`;
 export const ENQUIRY_URL = `${BASE_URL}api/enquiries/`;
 export const CONTACT_URL = `${BASE_URL}api/contacts/`;
